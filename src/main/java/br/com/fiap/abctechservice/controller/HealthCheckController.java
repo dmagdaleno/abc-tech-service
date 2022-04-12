2e91fbb8ffc7d08a04ae36a36d5a1631f5fb06cf
dmagdaleno <diegommagdaleno@gmail.com>
2022-04-11 22:54:20 -0300

    name and version

--- src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
@@ -12,5 +15,17 @@ public class HealthCheckController {
     @GetMapping
     public ResponseEntity<String> status() {
         return ResponseEntity.ok("Server is up");
+    }
+
+    @GetMapping("/version")
+    public ResponseEntity<String> version() {
+        final var properties = new Properties();
+        final var is = getClass().getClassLoader().getResourceAsStream("application.yaml");
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(String.format("%s - %s", properties.getProperty("name"), properties.getProperty("version")));
     }
 }
