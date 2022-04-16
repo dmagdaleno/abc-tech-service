853fef1b0207e725e92670ddebbeed62af9d184f
dmagdaleno <diegommagdaleno@gmail.com>
2022-04-15 22:26:25 -0300

    create order model

---
+++ src/main/java/br/com/fiap/abctechservice/model/OrderLocation.java
@@ -0,0 +1,29 @@
+package br.com.fiap.abctechservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+    private Date date;
+}
